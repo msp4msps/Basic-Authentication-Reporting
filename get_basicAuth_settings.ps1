55b83b1caf8bee398cb6bb64e2c2dfe673aaec64
msp4msps <msp4msps@tminus365.com>
2021-10-04 10:40:33 -0600

    Removed variable

--- get_basicAuth_settings.ps1
+++ get_basicAuth_settings.ps1
@@ -61,7 +61,6 @@ foreach ($customer in $customers) {
     $token = New-PartnerAccessToken -ApplicationId 'a0c73c16-a7e3-4564-9a95-2bdf47383716'-RefreshToken $ExchangeRefreshToken -Scopes 'https://outlook.office365.com/.default' -Tenant $customer.TenantId
     $tokenValue = ConvertTo-SecureString "Bearer $($token.AccessToken)" -AsPlainText -Force
     $credential = New-Object System.Management.Automation.PSCredential($upn, $tokenValue)
-    $customerId = $customer.DefaultDomainName
     $InitialDomain = Get-MsolDomain -TenantId $customer.TenantId | Where-Object {$_.IsInitial -eq $true}
     $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell-liveid?DelegatedOrg=$($InitialDomain)&BasicAuthToOAuthConversion=true" -Credential $credential -Authentication Basic -AllowRedirection 
     try{
